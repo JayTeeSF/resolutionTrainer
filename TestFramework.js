dc43e4a3b439f2c2da6abd7957f77267baf10af9
JayTeeSF <jaytee@jayteesf.com>
2023-12-22 13:31:08 -0800

    works in UI and console

--- TestFramework.js
+++ TestFramework.js
@@ -1,5 +1,3 @@
-// TestFramework.js
-
 const TestFramework = {
   testCount: 0,
   passCount: 0,
@@ -21,6 +19,7 @@ const TestFramework = {
       }
     }
     this.printSummary();
+    this.displayTestResults(); // Call the new method to update UI
   },
 
   assert: function (condition, message) {
@@ -35,10 +34,32 @@ const TestFramework = {
     console.log(`  Passed: ${this.passCount}`);
     console.log(`  Failed: ${this.failCount}`);
   },
-};
 
-// Export the TestFramework for use in other files
-// module.exports = TestFramework;
+  displayTestResults: function () {
+    const resultsContainer = document.getElementById('test-results');
+    if (!resultsContainer) return;
+
+    resultsContainer.innerHTML = `
+      <div class="mb-4">
+        <span class="font-bold">Total Tests:</span> ${this.testCount}
+      </div>
+      <div class="mb-4">
+        <span class="font-bold text-green-500">Passed:</span> ${this.passCount}
+        ${this.passCount > 0 ? '<i class="fas fa-check-circle text-green-500"></i>' : ''}
+      </div>
+      <div class="mb-4">
+        <span class="font-bold text-red-500">Failed:</span> ${this.failCount}
+        ${this.failCount > 0 ? '<i class="fas fa-times-circle text-red-500"></i>' : ''}
+      </div>
+    `;
+
+    if (this.failCount === 0) {
+      resultsContainer.innerHTML += '<div class="text-green-500"><i class="fas fa-check-circle"></i> All tests passed!</div>';
+    } else {
+      resultsContainer.innerHTML += '<div class="text-red-500"><i class="fas fa-times-circle"></i> Some tests failed. See console for details.</div>';
+    }
+  },
+};
 
 // Attach TestFramework to the global window object
 window.TestFramework = TestFramework;
