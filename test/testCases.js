e5088fa84f075232891028370c097624b9805e29
JayTeeSF <jaytee@jayteesf.com>
2023-12-22 14:47:24 -0800

    TDD 1a: first 3 function tests for game.js

--- test/testCases.js
+++ test/testCases.js
@@ -1,27 +1,26 @@
 // testCases.js
 
-// const TestFramework = require('./TestFramework');
-
-// Example function test
-function test_addition(assert) {
-  const result = productionCode.add(2, 3);
-  assert(result === 5, `Expected 5, but got ${result}`);
+function test_subtract(assert) {
+  const result = subtract(5, 3);
+  assert(result === 2, `Expected 2, got ${result}`);
 }
 
-// Example variable test
-function test_variable(assert) {
-  const value = productionCode.myVariable;
-  assert(value === 'Hello World', `Expected 'Hello World', but got ${value}`);
+function test_changeGameState(assert) {
+  const initial = document.getElementById('personalLevel');
+  changeGameState('personalLevel', 'communityLevel');
+  const newCurrent = document.getElementById('communityLevel');
+  assert(!initial.classList.contains('visible') && newCurrent.classList.contains('visible'), `Failed to change game state from personal to community`);
 }
 
-// Add more test functions for constants, classes, objects, etc.
+function test_resolveConflict(assert) {
+  const result = resolveConflict('player1', 'Resource Scarcity');
+  // Expected result should be based on game's conflict resolution mechanics
+  assert(result === expectedOutcome, `Unexpected conflict resolution outcome`);
+}
 
-// Export the test functions for use in other files
-// module.exports = { test_addition, test_variable, };
-
-// Attach test functions to the global window object
 window.testCases = {
-  test_addition,
-  test_variable,
-  // Add more test functions as needed
+  test_subtract,
+  test_changeGameState,
+  test_resolveConflict,
+  // other test cases...
 };
