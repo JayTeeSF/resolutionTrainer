b4ecd74d59cf6af5758563a7c5aa266401cd93c5
JayTeeSF <jaytee@jayteesf.com>
2023-12-22 18:57:49 -0800

    e2eTests.js load indicators

--- e2e/e2eTests.js
+++ e2e/e2eTests.js
@@ -1,74 +1,76 @@
 const E2ETestFramework = {
-    testCount: 0,
-    passCount: 0,
-    failCount: 0,
+  testCount: 0,
+  passCount: 0,
+  failCount: 0,
 
-    runTests: function (testModule) {
-        console.log('Starting E2E tests...');
-        for (const testName in testModule) {
-            if (testName.startsWith('test_') && typeof testModule[testName] === 'function') {
-                this.testCount++;
-                console.log(`Running test: ${testName}`);
-                try {
-                    testModule[testName](this.assert);
-                    this.passCount++;
-                    console.log(`${testName}: Passed`);
-                } catch (error) {
-                    this.failCount++;
-                    console.error(`${testName}: Failed - ${error}`);
-                }
-            }
+  runTests: function (testModule) {
+    console.log('Starting E2E tests...');
+    for (const testName in testModule) {
+      if (testName.startsWith('test_') && typeof testModule[testName] === 'function') {
+        this.testCount++;
+        console.log(`Running test: ${testName}`);
+        try {
+          testModule[testName](this.assert);
+          this.passCount++;
+          console.log(`${testName}: Passed`);
+        } catch (error) {
+          this.failCount++;
+          console.error(`${testName}: Failed - ${error}`);
         }
-        this.printSummary();
-    },
+      }
+    }
+    this.printSummary();
+  },
 
-    assert: function (condition, message) {
-        if (!condition) {
-            throw new Error(message);
-        }
-    },
+  assert: function (condition, message) {
+    if (!condition) {
+      throw new Error(message);
+    }
+  },
 
-    printSummary: function () {
-        console.log('\nTest Summary:');
-        console.log(`  Total Tests: ${this.testCount}`);
-        console.log(`  Passed: ${this.passCount}`);
-        console.log(`  Failed: ${this.failCount}`);
-    },
+  printSummary: function () {
+    console.log('\nTest Summary:');
+    console.log(`  Total Tests: ${this.testCount}`);
+    console.log(`  Passed: ${this.passCount}`);
+    console.log(`  Failed: ${this.failCount}`);
+  },
 
-    assertVisibility: function (elementId, isVisible, message) {
-        const element = document.getElementById(elementId);
-        if (!element) {
-            console.error(`Element with ID '${elementId}' not found.`);
-            throw new Error(`Element not found: ${elementId}`);
-        }
-        const displayStyle = getComputedStyle(element).display;
-        const condition = isVisible ? (displayStyle !== 'none') : (displayStyle === 'none');
-        if (!condition) {
-            throw new Error(message);
-        }
-    },
+  assertVisibility: function (elementId, isVisible, message) {
+    const element = document.getElementById(elementId);
+    if (!element) {
+      console.error(`Element with ID '${elementId}' not found.`);
+      throw new Error(`Element not found: ${elementId}`);
+    }
+    const displayStyle = getComputedStyle(element).display;
+    const condition = isVisible ? (displayStyle !== 'none') : (displayStyle === 'none');
+    if (!condition) {
+      throw new Error(message);
+    }
+  },
 };
 
 // Define your test cases here
 function test_subtract(assert) {
-    // Example test case
-    const result = 5 - 3;
-    assert(result === 2, `Expected 2, got ${result}`);
+  // Example test case
+  const result = 5 - 3;
+  assert(result === 2, `Expected 2, got ${result}`);
 }
 
 function test_startScreenVisibility(assert) {
-    console.log('Testing start screen visibility...');
-    E2ETestFramework.assertVisibility('startScreen', true, 'Start screen should be visible on load');
+  console.log('Testing start screen visibility...');
+  E2ETestFramework.assertVisibility('startScreen', true, 'Start screen should be visible on load');
 }
 
 window.testCases = {
-    test_subtract,
-    test_startScreenVisibility,
-    // Add more test cases here
+  test_subtract,
+  test_startScreenVisibility,
+  // Add more test cases here
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() => {
-        E2ETestFramework.runTests(window.testCases);
-    }, 1000); // Delay to ensure DOM is fully loaded
+  setTimeout(() => {
+    console.log("e2eTests.js domcontentloaded and timeout done");
+    E2ETestFramework.runTests(window.testCases);
+  }, 1000); // Delay to ensure DOM is fully loaded
 });
+console.log("e2eTests.js loaded");
