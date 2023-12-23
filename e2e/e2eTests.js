ab52d7019740804ba89ed10ab73c3d4e5c9e999a
JayTeeSF <jaytee@jayteesf.com>
2023-12-22 16:30:26 -0800

    broken e2e/index.html

---
+++ e2e/e2eTests.js
@@ -0,0 +1,39 @@
+// e2eTests.js
+const E2ETestFramework = {
+    runTest: function (testName, testFunction) {
+        try {
+            testFunction();
+            console.log(`${testName}: Passed`);
+        } catch (error) {
+            console.error(`${testName}: Failed - ${error.message}`);
+        }
+    },
+
+    assertVisibility: function (elementId, isVisible, message) {
+        const element = document.getElementById(elementId);
+        const displayStyle = getComputedStyle(element).display;
+        const condition = isVisible ? (displayStyle !== 'none') : (displayStyle === 'none');
+        if (!condition) {
+            throw new Error(message);
+        }
+    },
+
+    // Define your test cases here
+    testStartScreenVisibility: function () {
+        this.assertVisibility('startScreen', true, 'Start screen should be visible on load');
+        // Simulate click or other actions to change state
+        // this.assertVisibility('otherDiv', false, 'Other Div should be hidden after action');
+    },
+
+    // Add more tests as needed
+
+    runAllTests: function () {
+        this.runTest('Start Screen Visibility', this.testStartScreenVisibility.bind(this));
+        // Add more test runs here
+    }
+};
+
+// Run the tests when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+    E2ETestFramework.runAllTests();
+});
