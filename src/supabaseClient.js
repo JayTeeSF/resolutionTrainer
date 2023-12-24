04ad156000962dbf4fe8923b4304867f906e3dda
JayTeeSF <jaytee@jayteesf.com>
2023-12-24 11:37:24 -0600

    no supabase ...via web

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
+/*
+// import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'YOUR_SUPABASE_URL';
 const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
@@ -6,3 +7,4 @@ const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 // Functions to interact with the database...
+*/
