ec316cfdb5c56450a9a34bbee2b7ef1fabf59422
JayTeeSF <jaytee@jayteesf.com>
2023-12-24 11:02:52 -0600

    add missing characters.js ref

---
+++ src/characters.js
@@ -0,0 +1,13 @@
+class Character {
+  constructor(name, background, sensitivityLevel, privilege) {
+    this.name = name;
+    this.background = background;
+    this.sensitivityLevel = sensitivityLevel;
+    this.privilege = privilege;
+  }
+
+  // Additional character methods...
+}
+
+// Example character initialization
+const character1 = new Character("Character1", "Cultural Background", "High", "Moderate");
